41db0a588215ffe0e9d69331bffac67630cbfb4d
walkerthetexasranger17-droid <walkerthetexasranger17@gmail.com>
2026-09-01 15:47:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this string every time you ship a new version of the app.
 // Bumping it forces the old cache to be thrown away and the new files fetched.
-const CACHE_VERSION = 'te-app-v18';
+const CACHE_VERSION = 'te-app-v19';
 const CORE_ASSETS = [
   './index.html',
   './manifest.json',
